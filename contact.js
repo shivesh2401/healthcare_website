7128d398ac9ed63eaa20ff1f7aae14d7ce855ac9
KUMAR PARTH <104761782+shivesh2401@users.noreply.github.com>
2022-12-28 16:50:08 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,18 @@
+function openForm() 
+           {
+           document.getElementById("popupForm").style.display = "block";
+           }
+
+        function closeForm() 
+           {
+            document.getElementById("popupForm").style.display = "none";
+           }
+
+        function dropdown() {
+    var x = document.getElementById("navbarResponsive");
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
