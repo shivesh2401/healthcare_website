04c9b0e5e34e9ce2e03911db0e507db4ab37e165
KUMAR PARTH <104761782+shivesh2401@users.noreply.github.com>
2022-12-28 16:57:19 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,8 @@
+function dropdown() {
+    var x = document.getElementById("navbarResponsive");
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
