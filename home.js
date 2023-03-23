e2864011876337b89e6e7e74ce3be37214e3ca09
KUMAR PARTH <104761782+shivesh2401@users.noreply.github.com>
2023-03-23 15:33:01 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+
 function dropdown() {
     var x = document.getElementById("navbarResponsive");
     if (x.style.display == 'none') {
@@ -6,3 +7,35 @@ function dropdown() {
         x.style.display = 'none';
     }
 }
+
+function openForm() 
+           {
+           document.getElementById("popupForm").style.display = "block";
+           }
+
+function closeForm() 
+           {
+            document.getElementById("popupForm").style. display = "none";
+           }
+
+function bookAppointment() {
+      // Get the form data
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var date = document.getElementById("date").value;
+      var time = document.getElementById("time").value;
+
+      // Validate the form data
+      if (name == "" || email == "" || date == "" || time == "") {
+        alert("Please fill in all the fields");
+        return;
+      }
+
+      // Send the appointment request to the server
+      // Here you would need to use an AJAX call to submit the form data to the server
+      // and handle the server response
+
+      // Show a confirmation message
+      alert("Appointment booked for " + name + " on " + date + " at " + time);
+    }
+
