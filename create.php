da406fbce4d690c8d1ff3d5a483e3a5fe47eb402
KUMAR PARTH <104761782+shivesh2401@users.noreply.github.com>
2023-03-23 15:35:57 +0530

    Update create.php

--- create.php
+++ create.php
@@ -1,17 +1,17 @@
 <?php
-$servername = "sql204.epizy.com";
-$username = "epiz_33639327";
+$servername = "localhost";
+$username = "username";
 $password = "";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
 // Create database
-$sql = "CREATE DATABASE epiz_33639327_suhasini12";
+$sql = "CREATE DATABASE contact";
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
